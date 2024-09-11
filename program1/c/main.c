5a308a45c63128f94254bf56d933e8ad58532863
Jason Carnahan <jac0081@uah.edu>
2024-09-11 12:06:38 -0500

    Read in inputfile

--- program1/c/main.c
+++ program1/c/main.c
@@ -1,7 +1,23 @@
 #include "stdio.h"
+#include "string.h"
 
 int main() {
-    printf("hello world\n");
+    // Get input file
+    printf("Please enter the input file location or \"1\" to assume \"./InputFile\"\n");
+    char fileLocation[128];
+    scanf("%s", fileLocation);
+    if (strcmp(fileLocation, "1") == 0) strcpy(fileLocation, "./InputFile");
+    FILE *inputFile = fopen(fileLocation,"r");
+    if (inputFile == NULL) {
+        printf("Unable to open file.\n");
+        return -1;
+    }
+
+    // Get file contents
+    char fileContents[64];
+    while (fgets(fileContents, sizeof(fileContents), inputFile)) {
+        printf("%s", fileContents);
+    }
 
     return 0;
 }
