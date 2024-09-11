d13054ae8ceeaeaccacba179914ebbdddaedc86c
Jason Carnahan <jac0081@uah.edu>
2024-09-11 12:59:07 -0500

    Added thread logic

--- program1/c/main.c
+++ program1/c/main.c
@@ -1,5 +1,12 @@
 #include "stdio.h"
 #include "string.h"
+#include "pthread.h"
+#include "stdlib.h"
+
+void *Scheduled_Process(void *threadid) {
+    printf("Hello Thread\n");
+    pthread_exit(NULL);
+}
 
 int main() {
     // Get input file
@@ -14,10 +21,26 @@ int main() {
     }
 
     // Get file contents
-    char fileContents[64];
-    while (fgets(fileContents, sizeof(fileContents), inputFile)) {
-        printf("%s", fileContents);
+    char fileContents[64][64];
+    int index = 0;
+    while (fgets(fileContents[index], sizeof(fileContents), inputFile)) {
+        printf("%s", fileContents[index]);
+        index++;
     }
 
+    // Create the threads and wait the time
+    int numOfThreads = index + 1;
+    int referenceCounter;
+    pthread_t threads[numOfThreads];
+
+    for (long i = 0; i < numOfThreads; i++) {
+        printf("Creating Thread %ld...\n", i);
+        referenceCounter = pthread_create(&threads[i], NULL, Scheduled_Process, (void *)i);
+        if (referenceCounter) {
+            printf("Error\n");
+            exit(-1);
+        }
+    }
+    pthread_exit(NULL);
     return 0;
 }
