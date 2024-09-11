40a8bfcd2186fed38c33b26528d917db0dd6d779
Jason Carnahan <jac0081@uah.edu>
2024-09-11 18:14:43 -0500

    C Threads and loop finished

--- program1/c/main.c
+++ program1/c/main.c
@@ -2,9 +2,28 @@
 #include "string.h"
 #include "pthread.h"
 #include "stdlib.h"
+#include <unistd.h>
 
-void *Scheduled_Process(void *threadid) {
-    printf("Hello Thread\n");
+struct Process {
+    long threadId;
+    char processName;
+    long waitTime;
+};
+
+void *Scheduled_Process(void *process) {
+    // Greet
+    struct Process *p = (struct Process *)process;
+    printf("Thread%ld: %s begins\n", p->threadId, &p->processName);
+
+    // Loop wait time
+    int numOfLoops = p->waitTime / 5;
+    for (int i = 0; i < numOfLoops; i++) {
+        printf("Thread%ld: Loop %d of %d loops total\n", p->threadId, i, numOfLoops);
+        usleep(5000);
+    }
+
+    // Close
+    printf("Thread%ld: %s ends\n", p->threadId, &p->processName);
     pthread_exit(NULL);
 }
 
@@ -22,27 +41,28 @@ int main() {
 
     // Get file contents
     char lineContent[32];
-    char processNames[32][4];
-    char processTime[32][10];
+    struct Process processes[32];
     int index = 0;
     while (fgets(lineContent, sizeof(lineContent), inputFile)) {
+        struct Process newProcess;
         char *token = strtok(lineContent, " "); // First Token of the line
-        strcpy(&processNames[index][0], token);
+        strcpy(&newProcess.processName, token);
         token = strtok(NULL, " "); // Second Token of the line
-        strcpy(&processTime[index][0], token);
+        char *endptr;
+        newProcess.waitTime = strtol(token, &endptr, 10);
+        printf("%ld\n", newProcess.waitTime);
+        processes[index] = newProcess;
         index++;
     }
 
-    return 0;
-
     // Create the threads and wait the time
-    int numOfThreads = index + 1;
+    int numOfThreads = index;
     int referenceCounter;
     pthread_t threads[numOfThreads];
 
     for (long i = 0; i < numOfThreads; i++) {
-        printf("Creating Thread %ld...\n", i);
-        referenceCounter = pthread_create(&threads[i], NULL, Scheduled_Process, (void *)i);
+        processes[i].threadId = i + 1; // Starting at 1
+        referenceCounter = pthread_create(&threads[i], NULL, Scheduled_Process, (void *)&processes[i]);
         if (referenceCounter) {
             printf("Error\n");
             exit(-1);
