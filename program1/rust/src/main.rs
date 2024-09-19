249e194572f524f2cf6b35474862a6bd955eda54
Jason Carnahan <jac0081@uah.edu>
2024-09-18 19:21:21 -0500

    Added comments and slight reorganization

--- program1/rust/src/main.rs
+++ program1/rust/src/main.rs
@@ -5,6 +5,63 @@ use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::thread::{self, sleep};
 
+/**
+ * Program written by Jason Carnahan as a Rust version of the CS 690 program 1.
+ * See the `program1.pdf` requirements document for details.
+ *
+ * The program limits the use of the `.unwrap()` for the sake of custom error handling.
+ */
+fn main() {
+    // Get input file and open for reading
+    let file_location = Path::new("InputFile");
+    let file = match File::open(&file_location) {
+        Ok(file) => file,
+        Err(why) => panic!("Couldn't open {}: {}", file_location.display(), why),
+    };
+
+    // Read in the lines of the file, parse name and wait time, and store in a vector
+    let mut process_vector: Vec<Process> = Vec::new();
+    let lines = io::BufReader::new(file).lines();
+    for (index, line) in lines.flatten().into_iter().enumerate() {
+        let split: Vec<&str> = line.split(" ").collect();
+
+        let process = Process {
+            thread_id: index + 1,
+            process_name: split[0].to_string(),
+            wait_time: match split[1].parse::<i32>() {
+                Ok(num) => num,
+                Err(_) => panic!("Could not parse wait time."),
+            },
+        };
+
+        process_vector.push(process);
+    }
+
+    // Open the output file and create and mutex that can safely pass among the threads
+    let output_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("OutputFile")
+        .expect("Failed accessing output file");
+    let writer = Arc::new(Mutex::new(output_file));
+
+    // Spawn a thread for each "process" and print the data using a Mutex
+    let num_of_threads = process_vector.len();
+    let mut thread_handles = vec![]; // Storage to wait for each thread to finish
+    for i in 0..num_of_threads {
+        let writer = Arc::clone(&writer); // A mutex for each thread
+        let process = process_vector[i].clone();
+        let thread_handle = thread::spawn(move || {
+            scheduled_process(process, &writer);
+        });
+        thread_handles.push(thread_handle);
+    }
+
+    for thread in thread_handles {
+        thread.join().unwrap();
+    }
+}
+
 #[derive(Clone)]
 struct Process {
     thread_id: usize,
@@ -13,6 +70,7 @@ struct Process {
 }
 
 fn scheduled_process(process: Process, writer: &Mutex<File>) {
+    // Greet the start of the thread
     writer
         .lock()
         .unwrap()
@@ -25,8 +83,9 @@ fn scheduled_process(process: Process, writer: &Mutex<File>) {
         )
         .expect("Could not write to outputfile.");
 
+    // Loop for the wait time and print status
     let num_of_loops = process.wait_time / 5;
-    for i in 0..num_of_loops {
+    for i in 1..num_of_loops + 1 {
         writer
             .lock()
             .unwrap()
@@ -41,6 +100,7 @@ fn scheduled_process(process: Process, writer: &Mutex<File>) {
         sleep(time::Duration::from_micros(5000));
     }
 
+    // End the thread
     writer
         .lock()
         .unwrap()
@@ -52,55 +112,4 @@ fn scheduled_process(process: Process, writer: &Mutex<File>) {
             .as_bytes(),
         )
         .expect("Could not write to outputfile.");
-}
-
-fn main() {
-    // Get input file and open for reading
-    let file_location = Path::new("InputFile");
-    let file = match File::open(&file_location) {
-        Ok(file) => file,
-        Err(why) => panic!("Couldn't open {}: {}", file_location.display(), why),
-    };
-
-    // Read in the lines of the file, parse name and wait time, and store in a vector
-    let mut process_vector: Vec<Process> = Vec::new();
-    let lines = io::BufReader::new(file).lines();
-    for line in lines.flatten() {
-        let split: Vec<&str> = line.split(" ").collect();
-
-        let process = Process {
-            thread_id: 0,
-            process_name: split[0].to_string(),
-            wait_time: match split[1].parse::<i32>() {
-                Ok(num) => num,
-                Err(_) => panic!("Could not parse wait time."),
-            },
-        };
-
-        process_vector.push(process);
-    }
-
-    // Spawn a thread for each "process" and print the data using a Mutex
-    let num_of_threads = process_vector.len();
-    let output_file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("OutputFile")
-        .expect("Failed accessing output file");
-    let writer = Arc::new(Mutex::new(output_file));
-    let mut thread_handles = vec![];
-
-    for i in 0..num_of_threads {
-        let writer = Arc::clone(&writer);
-        let mut process = process_vector[i].clone();
-        process.thread_id = i + 1;
-        let handle = thread::spawn(move || {
-            scheduled_process(process, &writer);
-        });
-        thread_handles.push(handle);
-    }
-
-    for handle in thread_handles {
-        handle.join().unwrap();
-    }
 }
